7bdeb7e60c41c9c01fd219e27a19caf7a449c0e9
Daniel Choi <dhchoi@gmail.com>
2012-06-20 21:47:09 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alertapp::Application.config.secret_token = 'dfc00060d562a5962048c683e173eab90659326be8503736e8e3b70875cd32158c5e3224da0d39f1b78a6f9496b4ea8d5506425eb802e806752a8c0cfe30f9b9'
